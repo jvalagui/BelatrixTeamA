7cf7aab4dd5500cf13604446962928842a69bc49
Claudia Castro <Claudia Castro>
2016-05-14 13:05:28 -0500

    cambios

--- Restaurante/src/main/java/com/lab/restaurante/Principal.java
+++ Restaurante/src/main/java/com/lab/restaurante/Principal.java
@@ -14,73 +14,102 @@ public class Principal {
     public static Scanner in = new Scanner(System.in);
     public static int opcion1;
     public static String opcion2;
-    public static Mesero mesero=new Mesero("","","",0);
-    public static Cliente cliente=new Cliente("","","",0);
+    public static Mesero mesero = new Mesero("", "", "", 0);
+    public static Cliente cliente = new Cliente("", "", "", 0);
 
-    public static void main(String[] args){
-        /*Cliente cliente1 = new Cliente("Bruce", "Whane", "1", Estados.EN_ESPERA);
+    public static void main(String[] args) {
+        Cliente cliente1 = new Cliente("Bruce", "Whane", "1", Estados.EN_ESPERA);
         Cliente cliente2 = new Cliente("Clark", "Kent", "2", Estados.EN_ESPERA);
-        Mesero mesero = new Mesero("Barrey", "Allen", "3", 1234567);
-        Mesa mesa = new Mesa(01,Estados.DESOCUPADA);*/
+        Mesero mesero1 = new Mesero("Barrey", "Allen", "3", 1234567);
+        Mesero mesero2 = new Mesero("Lex", "Luthor", "4", 123);
+        Mesero mesero3 = new Mesero("Tony", "Stark", "5", 1234);
 
-        do{
+
+        do {
             System.out.println("Bienvenido");
             System.out.println("Elija una opción");
             System.out.println("1.- Mesero");
             System.out.println("2.- Cliente");
             System.out.println("3.- Salir");
-            String opcion1String=in.next();
-            opcion1=Integer.parseInt(opcion1String);
+            String opcion1String = in.next();
+            opcion1 = Integer.parseInt(opcion1String);
 
             System.out.println("¿Qué desea hacer?");
-            System.out.println("a.- Crear");
-            System.out.println("b.- Leer");
-            System.out.println("c.- Actualizar");
-            System.out.println("d.- Borrar");
+            System.out.println("a");
+            System.out.println("b Leer");
+            System.out.println("c Actualizar");
+            System.out.println("d  Borrar");
             System.out.println("Ingrese una opcion --> ");
-            opcion2=in.next();
-        }while(opcion1!=3 || opcion1>=3);
+            opcion2 = in.next();
 
-        if(opcion1==1){
-            switch(opcion2){
-                case "a":
-                    break;
+            if (opcion1 == 1) {
+                switch (opcion2) {
 
-                case "b":
-                    break;
+                    case "a":
+                        System.out.println("Ingrese el nombre -->");
+                        String nombre=in.next();
+                        System.out.println("Ingrese el apellido ->");
+                        String apellido=in.next();
+                        System.out.println("Ingrese el id -->");
+                        String id=in.next();
+                        System.out.println("Ingrese el estado -->");
+                        String estadoString=in.next();
+                        int estado=Integer.parseInt(estadoString);
+                        Cliente cliente=new Cliente (nombre,apellido,id,estado);
+                        cliente.crear(cliente);
+                        System.out.println("Cliente agregado");
+                        break;
 
-                case "c":
-                    System.out.println("Ingrese ID --> ");
-                    String id=in.next();
-                    System.out.println("Ingrese nuevo DNI --> ");
-                    String nuevoDniString=in.next();
-                    long nuevoDni=Long.parseLong(nuevoDniString);
-                    mesero.update("3",nuevoDni);break;
+                    case "b":
+                        break;
 
-                case "d":
-                    break;
+                    case "c":
+                        System.out.println("Ingrese ID --> ");
+                        String id1 = in.next();
+                        System.out.println("Ingrese nuevo DNI --> ");
+                        String nuevoDniString = in.next();
+                        long nuevoDni = Long.parseLong(nuevoDniString);
+                        mesero.update("3", nuevoDni);
+                        break;
+
+                    case "d":
+                        break;
+
+                }
+            } else {
+                switch (opcion2) {
+                    case "a":
+                        System.out.println("Ingrese el nombre -->");
+                        String nombre=in.next();
+                        System.out.println("Ingrese el apellido ->");
+                        String apellido=in.next();
+                        System.out.println("Ingrese el id -->");
+                        String id=in.next();
+                        System.out.println("Ingrese el dni -->");
+                        String dni=in.next();
+                        long dni1 = Long.parseLong(dni);
+                        Mesero mesero=new Mesero (nombre,apellido,id,dni1);
+                        mesero.crear(mesero);
+                        System.out.println("Mesero agregado");
+                        break;
+
+                    case "b":
+                        break;
+
+                    case "c":
+                        System.out.println("Ingrese ID --> ");
+                        String id1 = in.next();
+                        System.out.println("Ingrese nuevo estado --> ");
+                        String nuevoEstadoString = in.next();
+                        int nuevoEstado = Integer.parseInt(nuevoEstadoString);
+                        cliente.update("3", nuevoEstado);
+                        break;
+
+                    case "d":
+                        break;
+                }
 
             }
-        }else{
-            switch(opcion2){
-                case "a":
-                    break;
-
-                case "b":
-                    break;
-
-                case "c":
-                    System.out.println("Ingrese ID --> ");
-                    String id=in.next();
-                    System.out.println("Ingrese nuevo estado --> ");
-                    String nuevoEstadoString=in.next();
-                    int nuevoEstado=Integer.parseInt(nuevoEstadoString);
-                    cliente.update("3",nuevoEstado);break;
-
-                case "d":
-                    break;
-            }
-
-        }
+        } while (opcion1 != 3 || opcion1 >= 3);
     }
 }
